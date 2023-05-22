c84f3d33e0adc1a8444b25125ada57854ecdec6b
zeus <zeuskeyz@gmail.com>
2023-05-22 09:35:09 +0300

    adds Squad properties to the Squad class

--- src/main/java/org/example/model/Squad.java
+++ src/main/java/org/example/model/Squad.java
@@ -1,4 +1,47 @@
 package org.example.model;
 
 public class Squad {
+    private  String squad;
+    private  String cause;
+    private  Integer size;
+
+    public String getSquad() {
+        return squad;
+    }
+
+    public void setSquad(String squad) {
+        this.squad = squad;
+    }
+
+    public String getCause() {
+        return cause;
+    }
+
+    public void setCause(String cause) {
+        this.cause = cause;
+    }
+
+    public Integer getSize() {
+        return size;
+    }
+
+    public void setSize(Integer size) {
+        this.size = size;
+    }
+
+
+    public Squad(String squad, String cause, Integer size) {
+        this.squad = squad;
+        this.cause = cause;
+        this.size = size;
+    }
+
+    @Override
+    public String toString() {
+        return "Squad{" +
+                "squad='" + squad + '\'' +
+                ", cause='" + cause + '\'' +
+                ", size=" + size +
+                '}';
+    }
 }

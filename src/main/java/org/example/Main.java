db99874602872c485fd20007e458e6eca0b0dadb
zeus <zeuskeyz@gmail.com>
2023-05-22 10:11:46 +0300

    added code to the main class and added handlebar templates and image files to the resources folder

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,136 @@
 package org.example;
 
+import org.example.dao.HeroDao;
+import org.example.dao.SquadDao;
+import org.example.model.Hero;
+import org.example.model.Squad;
+
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.*;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
-public class Main {
+
+public abstract class Main {
+
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        staticFileLocation("/public");
+        HandlebarsTemplateEngine views = new HandlebarsTemplateEngine();
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        //LANDING PAGE
+        get("/", (req,res) -> new ModelAndView(new HashMap<>(),"landing-page.hbs"), views );
+
+        //HOME PAGE
+        get("/home", (req,res) -> {
+
+            Map<String, Object> combinedList = new HashMap<>();
+            combinedList.put("heroObject", HeroDao.getAllHeroes());
+            combinedList.put("squadObject", SquadDao.getAllSquads());
+            return new ModelAndView(combinedList, "home.hbs");
+
+        },views);
+
+        //HERO ADDING FORM PAGE
+        get("/add-hero", (req,res) -> new ModelAndView(new HashMap<>(),"add-hero.hbs"), views );
+
+        //SENDING HERO DETAILS TO DATABASE
+        post("/add-hero", (req,res)-> {
+
+            String hero = req.queryParams("hero");
+            Integer age = Integer.parseInt(req.queryParams("age"));
+            String power = req.queryParams("power");
+            Integer power_score = Integer.parseInt(req.queryParams("power_score"));
+            String weakness = req.queryParams("weakness");
+            Integer weakness_score = Integer.parseInt(req.queryParams("weakness_score"));
+            String squad = "";
+
+            Hero newHero = new Hero(hero.toUpperCase(),age,power,power_score,weakness,weakness_score,squad);
+            HeroDao.addHero(newHero);
+            res.redirect("/home");
+            return null ;
+        });
+
+        //SQUAD ADDING FORM PAGE
+        get("/add-squad", (req,res) -> new ModelAndView(new HashMap<>(),"add-squad.hbs"), views );
+
+        //SENDING HERO DETAILS TO DATABASE
+        post("/add-squad", (req,res)-> {
+
+            String squad = req.queryParams("squad");
+            String cause = req.queryParams("cause");
+            Integer size = Integer.parseInt(req.queryParams("size"));
+            Squad newSquad = new Squad(squad,cause,size);
+            SquadDao.addSquad(newSquad);
+            res.redirect("/home");
+            return null;
+
+        });
+
+        //HERO TO SQUAD ASSIGNMENT FORM
+        get("/assign/:squad", (req,res) -> {
+
+            String squad =  req.params("squad");
+
+            if(HeroDao.heroCount(squad) < SquadDao.maxSize(squad)) {
+                HeroDao.membership(squad);
+            } else {res.redirect("/full-squad");}
+            Map<String, Object> combinedList = new HashMap<>();
+            combinedList.put("querySquad", squad);
+            combinedList.put("heroObject", HeroDao.membership(squad));
+
+            return new ModelAndView(combinedList, "assign-squad.hbs");
+        },views);
+
+        //ASSIGNING A HER0 TO A SQUAD
+        post("/assign/:squad", (req,res) -> {
+
+            String hero = req.queryParams("hero");
+            String squad = req.queryParams("squad");
+            HeroDao.updateMembership(hero, squad);
+            res.redirect("/home");
+            return null;
+
+        },views);
+
+        //DELETING A HERO FROM THE PAGE
+        get("/delete-hero/:hero", (req,res)-> {
+
+            String name = req.params(":hero");
+            HeroDao.deleteHero(name);
+            res.redirect("/home");
+            return null;
+
+        },views);
+
+        //DELETING A SQUAD FROM THE PAGE
+        get("/delete-squad/:squad", (req,res)-> {
+
+            String name = req.params(":squad");
+            SquadDao.deleteSquad(name);
+            res.redirect("/home");
+            return null;
+
+        },views);
+
+        //CREATES A PAGE WITH SEARCHABLE TABLE LIST OF ALL HEROES AND SQUADS
+        get("/all", (req,res) -> {
+
+            Map<String, Object> combinedList = new HashMap<>();
+            combinedList.put("heroObject", HeroDao.getAllHeroes());
+            combinedList.put("squadObject", SquadDao.getAllSquads());
+            return new ModelAndView(combinedList, "all.hbs");
+
+        },views);
+
+        //FULL SQUAD ERROR PAGE
+        get("/full-squad", (req,res) -> new ModelAndView(new HashMap<>(),"full-squad.hbs"), views );
+
     }
 }
+

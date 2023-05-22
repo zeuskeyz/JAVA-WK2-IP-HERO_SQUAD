00f66579dc46172ee20df405931c3192eeeeba2c
zeus <zeuskeyz@gmail.com>
2023-05-22 09:38:01 +0300

    database connection codes added to the database class

--- src/main/java/org/example/db/database.java
+++ src/main/java/org/example/db/database.java
@@ -1,4 +1,13 @@
 package org.example.db;
 
+import org.sql2o.Sql2o;
 public class database {
+    //DATABASE CONNECTION
+    private static final Sql2o connection = new Sql2o (
+
+            "jdbc:postgresql://localhost:5432/hero_squad",
+            "postgres",
+            "javaDB"
+    );
+    public static Sql2o getConnect() { return connection; }
 }
